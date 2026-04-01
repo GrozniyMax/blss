b5e2ddc00bf06bb146644e6480d9248eacce7513
Artem <v.amuzinsky@yandex.ru>
2026-04-01 15:50:57 +0300

    lab2: транзакции
    Signed-off-by: Artem <v.amuzinsky@yandex.ru>

---
+++ src/main/java/com/blss/blss/service/tx/TransactionExecutor.java
@@ -0,0 +1,25 @@
+package com.blss.blss.service.tx;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionOperations;
+
+import java.util.function.Supplier;
+
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class TransactionExecutor {
+
+    TransactionOperations transactionOperations;
+
+    public <T> T inTransaction(Supplier<T> action) {
+        return transactionOperations.execute(status -> action.get());
+    }
+
+    public void inTransaction(Runnable action) {
+        transactionOperations.executeWithoutResult(status -> action.run());
+    }
+}
