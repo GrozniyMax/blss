34d35eea3a82c917bfdb6c5470507bb0f2f69b25
GrozniyMax <AzmEcmTzar@mail.ru>
2026-04-04 13:40:26 +0300

    lab2: add custom exception handler

--- src/main/java/com/blss/blss/controller/ApiExceptionHandler.java
+++ src/main/java/com/blss/blss/controller/ApiExceptionHandler.java
@@ -7,16 +7,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.AccessDeniedException;
 import java.time.Instant;
 import java.util.UUID;
 
 @Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ErrorResponseDto handleAuthorizationDeniedException(AuthorizationDeniedException e, HttpServletRequest request) {
+        return build(e, request.getRequestURI());
+    }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
