5bce1ac7b0e20f51918a6da2b5fc70cbf6605c52
m.s.taranenko <m.s.taranenko@tbank.ru>
2026-04-04 12:24:28 +0300

    lab2: fix mapping

--- src/main/java/com/blss/blss/config/JacksonConfig.java
+++ src/main/java/com/blss/blss/config/JacksonConfig.java
@@ -1,10 +1,9 @@
 package com.blss.blss.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 /**
  * Jackson configuration for Java 8 date/time support.
@@ -13,9 +12,8 @@ import org.springframework.context.annotation.Primary;
 public class JacksonConfig {
 
     @Bean
-    @Primary
-    public ObjectMapper objectMapper(ObjectMapper defaultMapper) {
-        defaultMapper.registerModule(new JavaTimeModule());
-        return defaultMapper;
+    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+        return Jackson2ObjectMapperBuilder.json()
+                .modulesToInstall(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
     }
 }
