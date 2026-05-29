41b2f2ec018ac1462b8b828c13d9564689ec5920
m.s.taranenko <m.s.taranenko@tbank.ru>
2026-05-29 21:42:49 +0300

    lab3: fix quartz

--- order-service/src/main/java/com/blss/orderservice/config/QuartzConfig.java
+++ order-service/src/main/java/com/blss/orderservice/config/QuartzConfig.java
@@ -3,68 +3,32 @@ package com.blss.orderservice.config;
 import com.blss.orderservice.service.report.ReportQuartzJob;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
-import org.quartz.spi.TriggerFiredBundle;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
 @Slf4j
 @Configuration
 public class QuartzConfig {
 
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean(
-            ApplicationContext applicationContext
-    ) {
-        log.info("Creating Quartz SchedulerFactoryBean");
-        
-        SchedulerFactoryBean factory = new SchedulerFactoryBean();
-
-        factory.setJobFactory(new AutowiringSpringBeanJobFactory(applicationContext));
-
-        factory.setWaitForJobsToCompleteOnShutdown(true);
-        factory.setOverwriteExistingJobs(true);
-
-        return factory;
-    }
-
-    @Bean
     public JobDetail reportJobDetail() {
-        log.info("Creating ReportJob JobDetail");
         return JobBuilder.newJob(ReportQuartzJob.class)
-                .withIdentity("reportJob", "reporting")
-                .withDescription("Generates daily reports")
+                .withIdentity("dailyReportJob", "reporting")
+                .withDescription("Generates daily report via Spring Batch")
                 .storeDurably()
                 .build();
     }
 
     @Bean
-    public Trigger reportTrigger() {
-        log.info("Creating report trigger with cron: 0 * * * * ? (every minute)");
-        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0 0 0 * * ?")
-                .withMisfireHandlingInstructionFireAndProceed();
-
+    public Trigger reportTrigger(JobDetail reportJobDetail) {
         return TriggerBuilder.newTrigger()
-                .forJob(reportJobDetail())
-                .withIdentity("reportTrigger", "reporting")
-                .withDescription("Every minute")
-                .withSchedule(scheduleBuilder)
+                .forJob(reportJobDetail)
+                .withIdentity("dailyReportTrigger", "reporting")
+                .withDescription("Daily at 00:00")
+                .withSchedule(
+                        CronScheduleBuilder.cronSchedule("0 0 0 * * ?")
+                                .withMisfireHandlingInstructionFireAndProceed()
+                )
                 .build();
-    }
-
-    public static class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory {
-
-        private final ApplicationContext applicationContext;
-
-        public AutowiringSpringBeanJobFactory(ApplicationContext applicationContext) {
-            this.applicationContext = applicationContext;
-        }
-
-        @Override
-        protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
-            return applicationContext.getBean(bundle.getJobDetail().getJobClass());
-        }
     }
 }
--- order-service/src/main/java/com/blss/orderservice/service/report/ReportQuartzJob.java
+++ order-service/src/main/java/com/blss/orderservice/service/report/ReportQuartzJob.java
@@ -1,7 +1,6 @@
 package com.blss.orderservice.service.report;
 
 import com.blss.orderservice.config.ReportBatchConfig;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -9,17 +8,20 @@ import org.quartz.JobExecutionException;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.time.LocalDate;
 
 @Slf4j
-@Component
-@RequiredArgsConstructor
 public class ReportQuartzJob implements Job {
 
-    private final JobLauncher jobLauncher;
-    private final org.springframework.batch.core.Job reportJob;
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    @Qualifier("reportJob")
+    private org.springframework.batch.core.Job reportJob;
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
@@ -29,7 +31,7 @@ public class ReportQuartzJob implements Job {
 
             JobParameters params = new JobParametersBuilder()
                     .addString(ReportBatchConfig.DAY_PARAM, day.toString())
-                    .addLong("runAt", System.currentTimeMillis()) // уникальность запуска
+                    .addLong("runAt", System.currentTimeMillis())
                     .toJobParameters();
 
             jobLauncher.run(reportJob, params);
